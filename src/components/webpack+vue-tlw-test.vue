b786d7662937acb855820720affcf0ce9b765054
lanouhn <lanouhn@37.local>
2017-07-20 08:47:17 +0800

    仝

--- src/components/webpack+vue-tlw-test.vue
+++ src/components/webpack+vue-tlw-test.vue
@@ -1,78 +1,74 @@
 <template>
-	<div class="tlw-center-conts">
-		<div class="tlw-person-center">
-			<h2>个人中心</h2>
-			<dl v-for="item in cont">
-				<dt>{{item.dt}}<span>></span></dt>
-				<dd v-for="abc in item.dd">{{abc}}</dd>
-			</dl>
+	<div class="tlw-person-box">
+		<div class="tlw-person-order">
+			<h5>我的积分</h5>
+			<div class="tlw-myIntegration">
+				<p>当前积分：<em>240分</em></p>
+				<table>
+					<tr>
+						<th>积分日期</th>
+						<th>变动</th>
+						<th>来源</th>
+					</tr>
+					<tr>
+						<td>2015-04-15 16:20:35</td>
+						<td>+100</td>
+						<td>消费赠送</td>
+					</tr>
+				</table>
+			</div>
+			
 		</div>
+		
 	</div>
 </template>
 
 <script>
 	export default {
-		data() {
-			return {
-				cont: [{
-						dt: "交易管理",
-						dd: ["我的账户", "我的订单", "我的积分", "积分订单", "我的钱包", "充值卡兑换"]
-					},
-					{
-						dt: "会员资料",
-						dd: ["个人资料", "地址管理", "我的收藏", "最近浏览", "修改密码"]
-					},
-					{
-						dt: "站内信",
-						dd: ["我的消息", "意见反馈"]
-					}]
-			}
-		}
-		
+
 	}
 </script>
-<style scoped>
-	.tlw-center-conts {
+<style lang="less" scoped>
+	.tlw-person-box {
 		width: 1280px;
-		/*background: pink;*/
-		margin: 0 auto;
-	}
-	
-	.tlw-person-center {
-		width: 170px;
-		border: 1px solid #cccccc;
-		padding-left: 20px;
-		color: #666666;
-	}
-	
-	.tlw-person-center h2 {
-		font-size: 24px;
-		font-weight: 600;
-		border-bottom: 1px solid #cccccc;
-		line-height: 56px;
-	}
-	
-	.tlw-person-center dl {
-		margin: 10px 0;
-	}
-	
-	.tlw-person-center dl dt {
-		font-size: 19px;
-		font-weight: 600;
-	}
-	
-	.tlw-person-center dl dt span {
-		margin-left: 50px;
-	}
-	
-	.tlw-person-center dl dd {
-		font-size: 14px;
-		margin: 20px 0;
-		cursor: pointer;
-	}
-	
-	.tlw-person-center dl dd:hover {
-		color: #6ba361;
-		text-decoration: underline;
+		margin: 20px auto;
+		.tlw-person-order{
+			width: 1083px;
+			height: 618px;
+			border: 1px solid #CCCCCC;
+			margin-left: 25px;
+			position:relative;
+			h5{
+				font-size: 18px;
+				font-weight: 500;
+				color: #414141;
+				line-height: 60px;
+				border-bottom:1px solid #CCCCCC ;
+				padding-left: 20px;
+			}
+			.tlw-myIntegration{
+				padding: 20px 20px;
+				background: cornsilk;
+				p{
+					font-size: 17px;
+					font-weight: 600;
+					em{
+						font-size: 20px;
+						font-weight: 200;
+						color: #f08200;
+					}
+				}
+				table{
+					width: 100%;
+					border: 1px solid #cccccc;
+					tr{
+						border-bottom: 1px solid #cccccc;
+						display: flex;
+						justify-content: space-around;
+					}
+					
+				}
+			}
+		}
 	}
 </style>
