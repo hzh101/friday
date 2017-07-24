f4bcf0b12eec8120c45125552da0bc8aefe85301
lanouhn <lanouhn@37.local>
2017-07-24 20:46:00 +0800

    仝

--- src/components/index.vue
+++ src/components/index.vue
@@ -1,15 +1,11 @@
 <template>
 	<div>
-<<<<<<< HEAD
-		<cont-nav></cont-nav>
-		<router-view></router-view>
-=======
 	<cont-nav></cont-nav>
 	<keep-alive>
 		<router-view></router-view>
 	</keep-alive>
 >>>>>>> 97dd612336f714b238f3a738f12b1dcb166f0897
->>>>>>> 627c4b5a0b4b04d91ed2e438317d56ae28aac06e
+
 	</div>
 </template>
 
